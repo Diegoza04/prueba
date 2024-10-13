81c92d57ce70567403b4f83f7ff5590e9a00118e
Diegoza04 <diego.barraza@alumnos.uneatlantico.es>
2024-10-14 01:37:17 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace commits
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.Write("Hello World");
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
